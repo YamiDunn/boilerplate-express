e49aeefbbfca5a5600255f00875e3481c35587a9
YamiDunn <j.felipe.rodriguez@hotmail.com>
2023-02-27 21:03:13 -0500

    testing, json console log string of method+patch+ip

--- myApp.js
+++ myApp.js
@@ -7,6 +7,7 @@ app.get("/",(req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
 
+/*
 app.get("/json",(req, res) => {
     const response = {"message":""};    
     if (process.env.MESSAGE_STYLE === "uppercase") {
@@ -17,7 +18,15 @@ app.get("/json",(req, res) => {
         response.message = "Hello json";
     };
     return res.json(response);
+});*/
+
+app.get("/json",(req, res, next) => {
+const response = req.method + " " + req.path + " - " + req.ip;
+console.log(response);
+next();
 });
+
+
 
 
 app.use("/public", express.static(__dirname + "/public"));
